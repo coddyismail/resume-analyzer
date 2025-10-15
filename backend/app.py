11e42dade34bcc39fed3ae309b59fd9d26dead38
aakher <kymalum20@gmail.com>
2025-10-15 23:53:11 +0500

    idk

--- backend/app.py
+++ backend/app.py
@@ -25,5 +25,9 @@ def analyze():
 def serve_frontend():
     return send_from_directory(app.static_folder, "index.html")
 
+# import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
