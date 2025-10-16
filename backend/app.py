1babca4b07a9d38cd9dddf1b7ff9d256858ca51e
aakher <kymalum20@gmail.com>
2025-10-16 15:37:44 +0500

    hhjhh

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from analyzer import analyze_resume
 from parser import parse_resume
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder="../frontend", static_url_path="/")
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
     if "file" not in request.files:
         return jsonify({"error": "No file uploaded"}), 400
-
+    
     file = request.files["file"]
     text = parse_resume(file)
 
@@ -21,13 +21,6 @@ def analyze():
     result = analyze_resume(text)
     return jsonify(result)
 
-@app.route("/")
-def serve_frontend():
-    return send_from_directory(app.static_folder, "index.html")
-
-# import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
