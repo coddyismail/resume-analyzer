4804d8e858fbf61ca5f90dd7775fc16c5183815a
aakher <kymalum20@gmail.com>
2025-12-08 21:55:16 +0530

    9th try

--- backend/utils.py
+++ backend/utils.py
@@ -1,10 +1,8 @@
 import re
 
-
-
 def clean_text(text: str) -> str:
     text = text.lower()
-    text = re.sub(r"[^a-zA-Z0-9\s]", " ", text)
+    text = re.sub(r"[^a-zA-Z0-9@.\s+-]", " ", text)
     text = re.sub(r"\s+", " ", text)
     return text.strip()
 
@@ -12,7 +10,10 @@ def extract_emails(text: str):
     return re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", text)
 
 def extract_phone_numbers(text: str):
-    return re.findall(r"(\+?\d{1,3}[-.\s]??\d{3,5}[-.\s]??\d{5,7})", text)
+    return re.findall(
+        r"(\+?\d{1,3}[-.\s]?\d{3,5}[-.\s]?\d{3,5}[-.\s]?\d{2,4})",
+        text
+    )
 
 def word_count(text: str) -> int:
     return len(text.split())
